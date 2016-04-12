54ed482b10de60ac4529083b7a9e0a0e2b0e55b8
Mithun Selvaratnam <mselvaratnam@gmail.com>
2016-04-11 22:55:57 -0400

    class8

--- class8/blackjacklevel1.rb
+++ class8/blackjacklevel1.rb
@@ -1,41 +1,17 @@
-#BIG ISSUE: How do I get the Game class and the methods within it to recognize the instance variable that I defined within the player class. I'm specifically talking about @money and @bet_amt. When I try to use them in 'Game' methods, I just get blanks.
-
-class Player
-  def initialize(money)
-    $money=money
-  end
-
-  def bet
-    puts "You have $#{$money} remaining. How much would you like to bet?" #include check for overbetting
-      bet_amt = gets.chomp.to_f
-        if bet_amt==0.0
-          puts "Please enter a valid number."
-          bet
-        else
-          $bet_amt = bet_amt
-          #puts @money-@bet_amt
-        end
-  end
-end
-
 class Game
-  def initialize(player)
-    @pl = player
+  def initialize
     @deck_rank = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Ace","King","Queen","Jack"]
     @deck_suit = %w[Clubs Diamonds Spades Hearts]
       puts "Welcome to Blackjack."
-      @pl.bet
-      puts $money
-      puts $bet_amt
       round
   end
-  def round #deals both hands, and starts player's turn
+  def round
     puts "A new round begins."
     playerdeal
     compdeal
     playerturn
   end
-  def drawcard #draws a random card and puts it in an array.
+  def drawcard
     @drawn_val=@deck_rank.shuffle[0]
     @card=Array.new
       @card << "the #{@drawn_val} of #{@deck_suit.shuffle[0]}"
@@ -48,17 +24,17 @@ class Game
       @drawn_val=10
     end
   end
-  def playerdraw #draws a card and adds it to player's hand and value arrays
+  def playerdraw
     drawcard
     @player_value<<@drawn_val
     @player_hand<<@card
   end
-  def compdraw #does what playerdraw does for the dealer
+  def compdraw
     drawcard
     @dealer_value<<@drawn_val
     @dealer_hand<<@card
   end
-  def playerdeal #creates card and value arrays for the player, draws, and reports.
+  def playerdeal
     @player_hand=Array.new
     @player_value=Array.new
       2.times do
@@ -67,16 +43,14 @@ class Game
     puts "Your cards are #{@player_hand.join(', ')}."
     puts "The value of your hand is #{@player_value.reduce(:+)}."
   end
-  def compdeal #playerdeal for the dealer
+  def compdeal
     @dealer_hand=Array.new
     @dealer_value=Array.new
       compdraw
         puts "The dealer's cards are #{@dealer_hand.join(', ')} and a hole card."
           compdraw
   end
-  def playerturn #check player's hand value and allows option of hitting if under 21
-    #puts @money
-    #puts @bet_amt
+  def playerturn
     if @player_value.reduce(:+)<21
       puts "Would you like to hit or stand?"
         ans=gets.chomp.downcase
@@ -86,23 +60,19 @@ class Game
             compturn
           end
     elsif @player_value.reduce(:+)==21
-      puts "Lucky you. 21, right on the money." #automatically goes to dealer's turn
+      puts "Lucky you. 21, right on the money."
         compturn
     else
-      puts "Ooh, you went over. Sorry, but you lose." #ends round
-        $money-=$bet_amt
-        puts "You have $#{$money} left." #why is @money empty?
-        @pl.bet
-        round  #include money loss
+      puts "Ooh, you went over. Sorry, but you lose."
     end
   end
-  def hit #draws and reports
+  def hit
     playerdraw
       puts "You drew #{@card}."
       puts "Your cards are now #{@player_hand.join(', ')}, and their total value is #{@player_value.reduce(:+)}."
       playerturn
   end
-  def compturn #automatically redraws until dealer's hand is over 16
+  def compturn
       if @dealer_value.reduce(:+)<17
         compdraw
           puts "The dealer drew #{@card}."
@@ -110,13 +80,12 @@ class Game
             compturn
       elsif @dealer_value.reduce(:+)>21
         puts "The dealer overdrew! You win the round."
-          round #include money gain
       else
         puts "The dealer's hand has a value of #{@dealer_value.reduce(:+)}."
           showdown
       end
   end
-  def showdown #activates only if no one overdrew. compares values.
+  def showdown
     if @player_value.reduce(:+)>@dealer_value.reduce(:+)
       puts "You won the round!"  #money gain
     elsif @dealer_value.reduce(:+)>@player_value.reduce(:+)
@@ -127,5 +96,4 @@ class Game
   end
 end
 
-@pl = Player.new(100)
-new_g=Game.new(@pl)
+Game.new
